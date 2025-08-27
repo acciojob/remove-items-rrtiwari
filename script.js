e0d3c2e46dba0f1e11052b7abd4d1047cd0bf0f1
Rahul Tiwari <128848653+rrtiwari@users.noreply.github.com>
2025-08-27 10:29:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+function removeColor() {
+      let select = document.getElementById("colorSelect");
+      let selectedIndex = select.selectedIndex;  
+
+      if (selectedIndex !== -1) {  
+        select.remove(selectedIndex); 
+      }
+    }
